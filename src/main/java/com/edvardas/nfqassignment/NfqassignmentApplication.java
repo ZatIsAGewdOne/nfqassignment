8fb3b2139a509532882a7f40bf1d64993dc89cd5
Edvardas Malinauskas <edva331@gmail.com>
2021-04-13 14:21:27 +0300

    Initial project

---
+++ src/main/java/com/edvardas/nfqassignment/NfqassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.edvardas.nfqassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfqassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfqassignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edvardas/nfqassignment/NfqassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edvardas.nfqassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfqassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
